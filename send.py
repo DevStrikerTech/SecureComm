9d0c389dd62f7c4532f025790219c55207e7ec0b
grzmot22 <grzmot22@users.noreply.github.com>
2019-03-06 11:43:39 +0000

    Add another parser and filling missing variables

--- send.py
+++ send.py
@@ -10,8 +10,9 @@ logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
 
 parser.add_argument('-g', dest='gpio', type=int, default=27,
                     help="GPIO pin (Default: 17)")
-parser.add_argument('-p', dest'protocol', type=, default=, help="Protocol. Default:")
-parser.add_argument('-t', dest'pulselength', type=, default=, help="Pulse length")
+parser.add_argument('-p', dest='protocol', type=int, default=1, help="Protocol. Default: 1")
+parser.add_argument('-t', dest='pulselength', type=int, default=350, help="Pulse length, Default: 350")
+parser.add_argurment()'code', metavar='CODE, type=int, help="Decimal code to send") 
 
 rfdevice = RFDevice(args.gpio)
 rfdevice.enable_tx()
