631b9092b30273e5b7d1e8707deefd5e275c028c
Grzmot22 <grzmot22@gmail.com>
2019-03-14 17:12:58 +0000

    Add working parser to choose the GPIO pin

--- receive.py
+++ receive.py
@@ -15,6 +15,10 @@ def exithandler(signal, frame):
     rfdevice.cleanup()
     sys.exit(0)
 
+parser = argparse.ArgumentParser(description='Receive a decimal code from the target device')
+parser.add_argument('-g', dest='gpio', type=int, default=27,
+                    help="GPIO pin (Default: 27)")
+args = parser.parse_args()
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                     format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
 
@@ -22,7 +26,11 @@ signal.signal(signal.SIGINT, exithandler)
 rfdevice = RFDevice(args.gpio)
 rfdevice.enable_rx()
 timestamp = None
-logging.info("Listening for codes on GPIO " + str(args.gpio))
+if args.gpio:
+    gpio = args.gpio
+else:
+    gpio = "default"
+logging.info("Listening for codes on GPIO " + str(gpio))
 while True:
     if rfdevice.rx_code_timestamp != timestamp:
         timestamp = rfdevice.rx_code_timestamp
